45f4eacc4ad7226144affe1832a4b01139496988
GideonGichamba <gideongichamba@gmail.com>
2022-09-20 11:09:48 -0700

    number 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for number in range(100):
-    if int(number/10):
-        print("{}".format(x), end=", ")
-    else:
-        print("{}".format(x))
-~
-
+for i in range(10):
+    for j in range(i, 10):
+        if i < j:
+            print("{:d}{:d}".format(i, j),
+                  end="\n" if i is 8 and j is 9 else ", ")
